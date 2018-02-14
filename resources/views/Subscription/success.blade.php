a5be7b79858941a6f455e5a5d5a9b981340c5d74
taqfu <taqfu0@gmail.com>
2018-02-14 21:21:39 +0000

    Cosmetic changes

--- resources/views/Subscription/success.blade.php
+++ resources/views/Subscription/success.blade.php
@@ -1,9 +1,9 @@
 @extends ('layouts.app')
 @section ('content')
     <div id='middle-container'>
-        <div id='title' class='text-center'>Remember death</div>
+        <div id='title' class='text-center'>Death is inescapable</div>
         <div class='text-center'>
-            But don't worry, we'll help you remember now. </br>(Technology is awesome like that.)
+            But don't worry, we won't let you forget now that you've subscribed. </br>(Technology is awesome like that.)
         </div>
     </div>
 @endsection
